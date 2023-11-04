fb97473080e0a3d8a182a700a3b50e28a868dea8
Venus <venusdragon718@gmail.com>
2023-11-04 23:30:45 +0900

    Card Component is created for only UI

---
+++ src/components/Card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+</script>
+
+<div class="mb-2 rounded border-solid bg-gray-100 shadow-lg flex flex-row relative items-center max-w-sm">
+    <input class="text-gray-600 mx-4 px-1 w-32" />
+    <span class="text-lg font-bold px-4 absolute centerize">0</span>
+    <div class="ml-auto">
+        <button class="btn bg-red-500 rounded-l">+</button>
+        <button class="btn bg-blue-500">-</button>
+        <button class="btn bg-yellow-500 rounded-r">0</button>
+        <span class="text-gray-500 cursor-pointer px-4">x</span>
+    </div>
+</div>
+
+<style>
+
+</style>
